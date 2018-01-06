8caa8cdb902d2bbd0cb57ef1fd6729b4596adf08
root <root@localhost.localdomain>
2018-01-07 02:54:32 +0530

    Few changes in the scripts

--- autoplay.sh
+++ autoplay.sh
@@ -2,17 +2,25 @@
 clear
 printf "\n\t\tThis Script install GIT MAVEN JENKINS ANSIBLE & DOCKER \n\n"
 printf " \t\t  It only works for %s Family\n\n" "RedHat"
-printf "\n\n\n\n"
+printf "\n"
 printf "\n\t\t\t Maintainer PRAKASH POUDEL"
 printf "\n\n\n\n"
 sleep 3
 
 scripts/installing-java.sh
-
-sudo yum update -y
 clear
+printf "\n\n\n\n"
+echo " UPDATING YOUR SYSTEM"
+echo "UPDATING YOUR REPO GIVES THE LATEST PACKAGES"
+sleep 5
+suoo yum update -y
+clear
+echo " Installing the following Package"
+printf "\n\tGIT\n\tMAVEN\n\tANSIBLE\n\tJENKINS\n\tAnd all their Dependencies needed . . ."
+sleep 10
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
 sudo yum install -y git \
                maven \
                epel-release \
@@ -23,9 +31,22 @@ sudo yum install -y git \
                python-devel \
                python-pip
 
-sudo systemctl restart jenkins \
-                       docker
+sudo systemctl restart jenkins
+sudo systemctl restart docker
 clear
-echo " You are all set "
+echo "Following Packages are installed:"
+printf "\n\tGIT\n\tMAVEN\n\tANSIBLE\n\tJENKINS\n\tAnd all their Dependencies needed . . .\n\n"
+sleep 3
+printf "\nYour JENKINS IS UPTO DATE , YOU CAN BROWSE IT IN PORT 8080\n"
+sleep 5
+printf "\n Your ANSIBLE is all set, but you still you need to configure HOST file\n"
+echo "The host file is located in /etc/ansible/hosts"
+printf "\n\n========== You are all set==========\n\n"
 sleep 10
+echo " Your System is RESTARTING in 20 secs"
+echo " Press CTRL + C to bypass restarting"
+sleep 20
+printf "\n\nREBOOTING . . . . . . . . . . "
+sleep 3
+/usr/sbin/reboot
 
