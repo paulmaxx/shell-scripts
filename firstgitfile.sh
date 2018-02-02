c24605f82847e38269efdc0152eb8c6fdc5a3b29
paulmaxx <paulmaxx2015@gmail.com>
2018-02-03 03:54:17 +0530

    added files

---
+++ firstgitfile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#This is created for practice.
+
+echo "hello"
+ 
+
+ if [ find=home/susam/user ]
+read namaste
+
+ echo "hajur mere aaka"
+
+then
+   echo "hey we get this "home/susam/user" for you"
+
+  else 
+     echo "hey we cannot get this "home/susam/user"  for you"
+#read=namaste
+
+# echo "hajur mere aaka"
+
+ fi
+
+
